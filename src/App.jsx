6e52cbc3f72b2f8e50575f2823a45a99355c4c6e
karida-julia <karidajulia1@gmail.com>
2025-11-28 10:38:09 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import { useEffect, useState } from 'react'
 import s from './App.module.css'
 import {api} from "./api/api"
+import {Card} from './components/card'
 
 
 
 function App() {
   const [data, setData] = useState([])
+  const [searchName, setSearchName] = useState([])
+  const [searchPage, setSearchPage] = useState([])
 
   useEffect(()=> {
-    api.get(`/character`).then((response) => {
+    api.get(`/character/?name=${searchName}&page=${searchPage}`).then((response) => {
       setData(response.data.results)
-      console.log(response.data.results)
     }).catch((error) => {
-      console.log("NÂO FOI POSSIVEL ACESSAR API", error)
+      console.error("NÂO FOI POSSIVEL ACESSAR API", error)
     })
-  }, [])
+  }, [searchPage, searchName])
 
   return (
     <>
       <h1 className={s.title}>Rick and Morty Characters</h1>
       <main>
-        <input type='text' value={''} onChange={''} placeholder='1/42'/>
-        <input type='text' value={''} onChange={''} placeholder='Procure um personagem'/>
+        <input type='text' value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
+        <input type='text' value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem'/>
+      
+        <div className={s.wrapCards}>
+          {data.map((item, index)  =>{
+            return(
+              <div key={index}>
+                <Card image={item.image} name={item.name} species={item.species}/>
+             
+              </div>
+
+          )
+          })}
+        
+        </div>
+
       </main>
     </>
   )
