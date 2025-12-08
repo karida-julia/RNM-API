a37a612e02baab74c17702ecb71c943943643eef
karida-julia <karidajulia1@gmail.com>
2025-12-08 11:45:05 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react'
 import s from './App.module.css'
-import {api} from "./api/api"
-import {Card} from './components/card'
-import Tilt from 'react-parallax-tilt'
+import { api } from "./api/api"
+import { Card } from './components/card'
 import InfoModal from './components/infoModal'
+import logo from '/logo.webp'
+import ResponsivePagination from 'react-responsive-pagination';
+import 'react-responsive-pagination/themes/classic-light-dark.css';
+
 
 
 function App() {
@@ -11,40 +14,70 @@ function App() {
   const [searchName, setSearchName] = useState("")
   const [searchPage, setSearchPage] = useState("")
   const [modal, setModal] = useState();
-  useEffect(()=> {
+  // const [loading, setLoading] = useState(true);
+  const [erro, setErro] = useState(false);
+
+  useEffect(() => {
     api.get(`/character/?name=${searchName}&page=${searchPage}`).then((response) => {
       setData(response.data.results)
+      // setLoading(false)
+      setErro(false)
     }).catch((error) => {
-      console.error("NÂO FOI POSSIVEL ACESSAR API", error)
+      if (error.response.status === 404) {
+        setErro(true)
+        console.error("Error, Characters Not Found!!!", error)
+      }
+      if (error.response.status === 500) {
+        setErro(true)
+        console.error("Error, Internal Server Error!!!", error)
+      }
+      console.error("Error, Could Not Get API!!!", error)
+      // setLoading(false)
+      setErro(true)
     })
   }, [searchPage, searchName])
-
   return (
 
     <>
     {modal !== undefined && <InfoModal data={data[modal]} close={() => setModal()}/>}
-      <h1 className={s.title}>Rick and Morty Characters</h1>
+      
+
       <main>
+          <img src={logo} alt="Logo" className={s.logo} />
+
+        <div className={s.wrapPagination}>
+          <ResponsivePagination
+            current={searchPage}
+            total={42}
+            onPageChange={setSearchPage}
+          />
+        </div>
+
         <div style={{display: "flex", flexWrap: "wrap", gap:"5px", alignItems: "center", justifyContent: "center"}}>
         <input type='text' value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
         <input type='text' value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem'/>
        </div>
 
-        <div className={s.wrapCards}>
-          {data.map((item, index)  =>{
-            return(
-              <div key={index}>
-              <Tilt tiltReverse={true} >
-                <Card image={item.image} name={item.name} species={item.species}/>
-              </Tilt>
-              <button onClick={() => setModal(index)} className={s.infoBtn}>Info {item.name}</button>
-              </div>
-          )
-          })}
-        
-        </div>
+       {erro &&
+          <div className={s.wrapErro}>
+            <h2>Error when searching for characters ⛔</h2>
+          </div>}
+
+      
+          <div className={s.wrapCards}>
+            {data.map((item, index) => {
+              return (
+                !erro && <div key={`${searchPage}-${searchName}-${index}`} className={s.wrapCard}>
+                  <Card image={item.image} name={item.name} species={item.species} />
+                  <button onClick={() => setModal(index)} className={s.infoBtn}>➕ Info {item.name}</button>
+                </div>
+              )
+            })}
+          </div>
+     
 
       </main>
+ 
     </>
   )
 }
